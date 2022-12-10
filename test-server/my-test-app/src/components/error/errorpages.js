80ed613c55847c8827f54f89f8b6ce0754cf20bc
sceddd <socdia07@gmail.com>
2022-12-10 22:03:46 +0700

    update database

--- test-server/my-test-app/src/components/error/errorpages.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-// launch this page if the url is not specified
-function errorpages() {
-  return (
-    <div>ERROR! Page not found</div>
-  )
-}
-
-export default errorpages
