8ae82ee781c6a5465e430ffc7df46e27f2c71dcd
nppt <105217439+npptGithub@users.noreply.github.com>
2022-12-19 17:53:32 +0700

    fix dbs

--- Database/FinalProjectDBs.sql
+++ Database/FinalProjectDBs.sql
@@ -7,8 +7,8 @@ go
 use FinalProjectSE
 go
 CREATE TABLE [Accountant] (
-  [ID] INT IDENTITY,
-  [AccountId] AS 'A' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY, -- A0000001
+  [ID] INT IDENTITY PRIMARY KEY,
+  [AccountId] AS 'A' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED, -- A0000001
   [name] varchar(50),
   [numPhone] varchar(10),
   [email] varchar(50),
@@ -18,55 +18,56 @@ CREATE TABLE [Accountant] (
 GO
 
 CREATE TABLE [ReceiveNote] (
-  [ID] INT IDENTITY,
-  [ReceiveID] AS 'RN' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY, -- RN000001
+  [ID] INT IDENTITY PRIMARY KEY,
+  [ReceiveID] AS 'RN' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED, -- RN000001
   [creator] varchar(50),
   [creatDay] varchar(8),
-  [accountantID] varchar(10)
+  [accountantID]  INT
 )
 GO
 
 CREATE TABLE [DeliveryNote] (
-  [ID] INT IDENTITY,
-  [DeliveryID] AS 'DN' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY, -- DN000001
+  [ID] INT IDENTITY PRIMARY KEY,
+  [DeliveryID] AS 'DN' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED , -- DN000001
   [creator] varchar(50),
   [creatDay] date default getDate(),
   [deliverStatus] int,
   [payStatus] int,
-  [accountantID] varchar(10)
+  [accountantID]  INT
 )
 GO
 
 CREATE TABLE [ProductInstance] (
-  [ID] INT IDENTITY,
-  [PIID] AS 'PI' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY, -- DN000001
+  [ID] INT IDENTITY PRIMARY KEY,
+  [PIID] AS 'PI' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED, -- DN000001
   [name] varchar(50),
   [price] float,
   [quantity] int,
   [totalPrice] float,
-  [cartID] varchar(10),
-  [productID] varchar(10)
+  [cartID] INT,
+  [productID]  INT
 )
 GO
 
 CREATE TABLE [ListOfProduct] (
-    [ID] INT IDENTITY,
-    [PIID] AS 'PI' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY, -- DN000001
-  [PIID] varchar(10),
+  [ID] INT IDENTITY PRIMARY KEY,
+  [PIID] AS 'PI' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED, -- DN000001
   [quantity] int,
   [totalPrice] float,
-  [noteID] varchar(10)
+  [noteID]  INT,
+  [prodInsID] INT
 )
 GO
 
 CREATE TABLE [Product] (
-  [ID] INT IDENTITY,
-  [ProductId] AS 'P' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY,
-  [supplierID] varchar(10),
+  [ID] INT IDENTITY PRIMARY KEY,
+  [ProductId] AS 'P' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED,
+  [supplierID] INT,
   [price] float,
   [genre] varchar(20)
 )
 GO
+
 
 CREATE TABLE [Genre] (
   [type] varchar(20) primary key
@@ -74,15 +75,15 @@ CREATE TABLE [Genre] (
 GO
 
 CREATE TABLE [Supplier] (
-  [ID] INT IDENTITY,
-  [supplierID] AS 'P' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY,
+  [ID] INT IDENTITY PRIMARY KEY,
+  [supplierID] AS 'P' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED,
   [supName] varchar(50)
 )
 GO
 
 CREATE TABLE [Customer] (
-  [ID] INT IDENTITY,
-  [CustomerId] AS 'C' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY,
+  [ID] INT IDENTITY PRIMARY KEY,
+  [CustomerId] AS 'C' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED,
   [name] varchar(50),
   [numPhone] varchar(10),
   [email] varchar(50),
@@ -92,50 +93,50 @@ CREATE TABLE [Customer] (
 GO
 
 CREATE TABLE [Order] (
-  [ID] INT IDENTITY,
-  [OrderId] AS 'O' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY,
+  [ID] INT IDENTITY PRIMARY KEY,
+  [OrderId] AS 'O' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED,
   [orderDay] varchar(10),
   [status] int,
   [payMethod] varchar(10),
-  [cusID] varchar(10)
+  [cusID]  INT
 )
 GO
 
 CREATE TABLE [Cart] (
-  [ID] INT IDENTITY,
-  [CartId] AS 'CA' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED PRIMARY KEY,
+  [ID] INT IDENTITY PRIMARY KEY,
+  [CartId] AS 'CA' + RIGHT('000000' + CAST(ID AS VARCHAR(10)), 6) PERSISTED,
   [numberOfItem] int,
   [totalPrice] float,
   [productName] varchar(50),
-  [orderID] varchar(10)
+  [orderID] INT
 )
 GO
 
 ALTER TABLE [Product] ADD FOREIGN KEY ([genre]) REFERENCES [Genre] ([type])
 GO
 
-ALTER TABLE [Product] ADD FOREIGN KEY ([supplierID]) REFERENCES [Supplier] ([id])
+ALTER TABLE [Product] ADD FOREIGN KEY ([supplierID]) REFERENCES [Supplier] ([ID])
 GO
 
 ALTER TABLE [ListOfProduct] ADD FOREIGN KEY ([prodInsID]) REFERENCES [ProductInstance] ([id])
 GO
 
-ALTER TABLE [ProductInstance] ADD FOREIGN KEY ([cartID]) REFERENCES [Cart] ([CartId])
+ALTER TABLE [ProductInstance] ADD FOREIGN KEY ([cartID]) REFERENCES [Cart] ([Id])
 GO
 
 ALTER TABLE [Order] ADD FOREIGN KEY ([cusID]) REFERENCES [Customer] ([id])
 GO
 
-ALTER TABLE [Cart] ADD FOREIGN KEY ([orderID]) REFERENCES [Order] ([OrderId])
+ALTER TABLE [Cart] ADD FOREIGN KEY ([orderID]) REFERENCES [Order] ([ID])
 GO
 
 ALTER TABLE [ProductInstance] ADD FOREIGN KEY ([productID]) REFERENCES [Product] ([id])
 GO
 
-ALTER TABLE [ReceiveNote] ADD FOREIGN KEY ([accountantID]) REFERENCES [Accountant] ([AccountId])
+ALTER TABLE [ReceiveNote] ADD FOREIGN KEY ([accountantID]) REFERENCES [Accountant] ([ID])
 GO
 
-ALTER TABLE [DeliveryNote] ADD FOREIGN KEY ([id]) REFERENCES [Accountant] ([AccountId])
+ALTER TABLE [DeliveryNote] ADD FOREIGN KEY ([id]) REFERENCES [Accountant] ([ID])
 GO
 
 ALTER TABLE [ListOfProduct] ADD FOREIGN KEY ([noteID]) REFERENCES [ReceiveNote] ([id])
